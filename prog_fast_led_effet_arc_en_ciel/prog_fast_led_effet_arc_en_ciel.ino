3fe4dbcf906e5fa4a777c55a65145973d44e82e9
Benjamin Mouëza <70459262+Moueben99@users.noreply.github.com>
2023-03-27 20:22:21 -0400

    modification du programme final de led

--- prog_fast_led_effet_arc_en_ciel/prog_fast_led_effet_arc_en_ciel.ino
+++ prog_fast_led_effet_arc_en_ciel/prog_fast_led_effet_arc_en_ciel.ino
@@ -4,35 +4,16 @@
 byte bright = 50; // luminosité des LEDs
 byte baza = 0;
 
-#define NUM_LEDS 165 // nombre de LEDs 100 par defaut
+#define NUM_LEDS 165 // nombre de LEDs
+#define PIN 4 // port de connexion ws2812b jaune
+#define PIN 7 // câble orange
 #define PIN 2 // câble bleu
 #define PIN 8 // câble vert
-#define PIN 4 // port de connexion ws2812b câble jaune
-#define PIN 7 // câble orange
 CRGB leds[NUM_LEDS];
-
-NEO_RGB leds1[118]
-NEO_RGB leds3[120]
-
-#define LED_PIN 
-#define LED_COUNT 120
-
-Adafruit_NeoPixel led1(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   FastLED.addLeds <WS2812, PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(bright);
-  
-  LED_PIN.addLeds<NEO_RGB, 2>(leds1, 118);
-  LED_PIN.addLeds<NEO_RGB, 8>(leds3, 120);
-
-  leds1.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  leds1.show();            // Turn OFF all pixels ASAP
-  leds1.setBrightness(250); // Set BRIGHTNESS to about 1/5 (max = 255)
-  
-  leds3.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  leds3.show();            // Turn OFF all pixels ASAP
-  leds3.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 void loop() {
@@ -43,7 +24,4 @@ void loop() {
     FastLED.setBrightness(bright);
     FastLED.show();
     delay(20);
-
-  leds1.show();
-  leds3.show();
 }
